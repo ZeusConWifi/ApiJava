d9d2e6e3027bc1b03aabd28f574ebe795dd8d2c1
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-12-16 23:43:38 -0300

    Add files via upload

---
+++ src/test/java/com/info21/ApiRestJava/ApiRestJavaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.info21.ApiRestJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
