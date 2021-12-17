3fb9c2e2d446887184964218b00bad310e85269d
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-12-17 14:35:54 -0300

    Delete src/main/java/com/info21/ApiRestJava directory

--- src/main/java/com/info21/ApiRestJava/entity/Estado.java
+++
@@ -1,7 +0,0 @@
-package com.info21.ApiRestJava.entity;
-
-public enum Estado {
-    ABIERTO,
-    EN_CURSO,
-    FINALIZADO
-}
--- src/main/java/com/info21/ApiRestJava/entity/Generado.java
+++
@@ -1,5 +0,0 @@
-package com.info21.ApiRestJava.entity;
-
-public enum Generado {
-    mobile, web, service;
-}
